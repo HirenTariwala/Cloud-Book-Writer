49f390f10b2051421b655c996b9a245c711255ef
Hiren Tariwala <41938818+HirenTariwala@users.noreply.github.com>
2023-10-02 13:44:05 +0530

    Update services.ts

--- components/Auth/Login/services.ts
+++ components/Auth/Login/services.ts
@@ -1,5 +1,5 @@
 export async function loginUser(payload: any) {
-    const response = await fetch('/login', {
+    const response = await fetch('http://localhost:5000/login', {
         method: 'POST',
         body: JSON.stringify(payload),
         headers: {
