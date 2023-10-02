8d3921edb6827b34551e16c7ecb368a88a4f3f67
Hiren Tariwala <41938818+HirenTariwala@users.noreply.github.com>
2023-10-02 14:52:02 +0530

    Update useHeader.ts

--- components/common/Header/useHeader.ts
+++ components/common/Header/useHeader.ts
@@ -1,13 +1,14 @@
 import { useContext } from 'react';
 import { useRouter } from 'next/navigation'
 import { AppContext } from '@/context/app.context'
+import { USER_LOGOUT } from '@/constants/actions';
 
 export default function useHeaderHook() {
     const router = useRouter()
     const { state, dispatch } = useContext(AppContext);
     
     const logout = async () => {
-        dispatch({ type: 'USER_LOGOUT' })
+        dispatch({ type: USER_LOGOUT })
         router.push('/login')
     }
 
