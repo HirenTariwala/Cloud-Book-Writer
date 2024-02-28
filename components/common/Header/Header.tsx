5c43753b1b39a12d24c63a0693ea40a1cd34323a
hirentariwala <hirentariwala19@gmail.com>
2024-02-28 16:01:10 +0530

    navbar buttons color fix

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,26 +1,54 @@
-'use client'
+"use client";
 
-import useHeaderHook from './useHeader'
+import useHeaderHook from "./useHeader";
 
 const Header = () => {
-    const { state, logout, router } = useHeaderHook()
-    return (
-        <div className='bg-[#1A1C1B] flex items-center justify-between px-6'>
-            <h1 className='text-center p-4 text-2xl'>Cloud Book Writer Platform</h1>
-            <div className='flex gap-6'>
-                {state?.token && <button onClick={() => router.push('/add-section')}>Add Section</button>}
-                {state?.user?.role === 'author' && <button onClick={() => router.push('/manage-access')}>Manage Access</button>}
-                {state.token && state?.token?.length > 0 ? (
-                    <button onClick={logout}>Log Out</button>
-                ) : (
-                    <>
-                        <button onClick={() => router.push('/login')}>Log In</button>
-                        <button onClick={() => router.push('/register')}>Sign Up</button>
-                    </>
-                )}
-            </div>
-        </div>
-    )
-}
+  const { state, logout, router } = useHeaderHook();
+  return (
+    <div className="bg-[#1A1C1B] flex items-center justify-between px-6">
+      <h1 className="text-white text-center p-4 text-2xl">
+        Cloud Book Writer Platform
+      </h1>
+      <div className="flex gap-6">
+        {state?.token && (
+          <button
+            onClick={() => router.push("/add-section")}
+            className="text-white"
+          >
+            Add Section
+          </button>
+        )}
+        {state?.user?.role === "author" && (
+          <button
+            onClick={() => router.push("/manage-access")}
+            className="text-white"
+          >
+            Manage Access
+          </button>
+        )}
+        {state.token && state?.token?.length > 0 ? (
+          <button onClick={logout} className="text-white">
+            Log Out
+          </button>
+        ) : (
+          <>
+            <button
+              onClick={() => router.push("/login")}
+              className="text-white"
+            >
+              Log In
+            </button>
+            <button
+              onClick={() => router.push("/register")}
+              className="text-white"
+            >
+              Sign Up
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
